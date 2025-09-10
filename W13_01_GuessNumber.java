76636ae1bc120808571bb54b6e7d5e89d6412464
Newza2588 Game <newza2588.p@gmail.com>
2025-09-10 10:08:22 +0700

    First Commit

---
+++ W13_01_GuessNumber.java
@@ -0,0 +1,19 @@
+public class W13_01_GuessNumber {
+    public static void main(String[] args) {
+        int numberToGuess,userGuess;
+
+        for(i=3;i == 0;i--;)
+        {
+        
+        
+        
+        if (userGuess < numberToGuess) {
+            System.out.println("Too low!");
+        } else if (userGuess > numberToGuess) {
+            System.out.println("Too high!");
+        } else {
+            System.out.println("Congratulations! You've guessed the number.");
+        }
+        }
+    }
+}
